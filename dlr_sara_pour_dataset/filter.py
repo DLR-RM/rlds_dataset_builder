f3572d084846b1851d5deaff1a393302e7905529
Abhishek Padalkar <Abhishek.Padalkar@dlr.de>
2023-08-30 16:09:45 +0200

    adding sanity check for data filtering

--- dlr_sara_pour_dataset/filter.py
+++ dlr_sara_pour_dataset/filter.py
@@ -6,12 +6,13 @@ from scipy.spatial.transform import Rotation
 
 if __name__ == "__main__":
     data_dir = "data/train/"
-    # save_dir = "data_filtered/train/"
+    save_dir = "data_filtered/train/"
     file_list = os.listdir(data_dir)
 
     for f in file_list:
         data = np.load(os.path.join(data_dir, f), allow_pickle=True)  # this is a list of dicts in our case
         l = len(data)
+        print(f"Episode: {f}")
         for i in range(l):
             if i < l - 1:
                 step_t = data[i]
@@ -20,20 +21,23 @@ if __name__ == "__main__":
                 # print(step['state'][0:3][np.newaxis].T)
                 h_msr = np.hstack((h_msr.as_matrix(), step_t['state'][0:3][np.newaxis].T))
                 h_msr = np.vstack((h_msr, np.array([0, 0, 0, 1])))
-                # print(h_msr)
+                # print(f"step_t[state]: {step_t['state']}")
+
                 h_des = Rotation.from_euler('zxy', step_t_1['state'][3:6])
                 h_des = np.hstack((h_des.as_matrix(), step_t_1['state'][0:3][np.newaxis].T))
                 h_des = np.vstack((h_des, np.array([0, 0, 0, 1])))
-
+                # print(f"step_t_1[state]: {step_t_1['state']}")
                 # For delta in Robot EEF frame
+
                 delta = np.linalg.inv(h_msr).dot(h_des)
                 # For delta in Robot base frame
                 # delta = h_des.dot(np.linalg.inv(h_msr))
+                delta = np.hstack((delta[:3, 3], Rotation.from_matrix(delta[:3, :3]).as_euler('zxy')))
+                # print(f"step_t[action]: {delta}")
 
-                delta = np.hstack((Rotation.from_matrix(delta[:3, :3]).as_euler('zxy'), delta[:3, 3]))
             else:
                 delta = np.zeros(6)
             step_t['action'] = delta
             data[i] = copy.deepcopy(step_t)
-        np.save(data, os.path.join(data_dir, f))
+        np.save(os.path.join(save_dir, f), data)
 
